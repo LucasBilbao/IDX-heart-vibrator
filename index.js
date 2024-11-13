653eb7751672978b2341a97010a35fad13142438
LucasBilbao <lucasbm789@gmail.com>
2024-11-13 13:26:05 +0100

    feat: implement beater vibration manipulation

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 const starter = document.querySelector('[data-start-haptic]');
+let heartInfo;
 
-const heartBeatPatterns = {
-  120: [500, 50, 500, 50],
-  121: [490, 50, 490, 50],
-  122: [480, 50, 480, 50],
-  123: [470, 50, 470, 50],
-  124: [460, 50, 460, 50],
-  125: [450, 50, 450, 50],
-  126: [440, 50, 440, 50],
-  127: [430, 50, 430, 50],
-  128: [420, 50, 420, 50],
-  129: [410, 50, 410, 50],
-  130: [400, 50, 400, 50],
-  131: [390, 50, 390, 50],
-  132: [380, 50, 380, 50],
-  133: [370, 50, 370, 50],
-  134: [360, 50, 360, 50],
-  135: [350, 50, 350, 50],
-  136: [340, 50, 340, 50],
-  137: [330, 50, 330, 50],
-  138: [320, 50, 320, 50],
-  139: [310, 50, 310, 50],
-  140: [300, 50, 300, 50],
-  141: [290, 50, 290, 50],
-  142: [280, 50, 280, 50],
-  143: [270, 50, 270, 50],
-  144: [260, 50, 260, 50],
-  145: [250, 50, 250, 50],
-  146: [240, 50, 240, 50],
-  147: [230, 50, 230, 50],
-  148: [220, 50, 220, 50],
-  149: [210, 50, 210, 50],
-  150: [200, 50, 200, 50],
-};
+function setUpPattern(data) {
+  let newHeartBeatPattern = [];
+  data.forEach((heart) => {
+    const systole = heart.rr / 3;
+    const diastole = (heart.rr * 2) / 3;
+    newHeartBeatPattern = [...newHeartBeatPattern, systole, diastole];
+  });
+
+  return newHeartBeatPattern;
+}
+
+fetch('./data.json')
+  .then((res) => res.json())
+  .then((data) => (heartInfo = data))
+  .catch(() => console.log('Something went wrong!!!'));
 
 const randomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+let isVibrating = false;
+let isOver = false;
+
+const INCREMENT_INDEX = 5;
+let prevIndex = 0;
+let nextIndex = INCREMENT_INDEX;
 
 starter.addEventListener('click', () => {
-  setInterval(() => {
-    const randIndex = randomNumberInRange(120, 151);
-    navigator.vibrate(heartBeatPatterns[randIndex]);
-  }, 1200);
+  if (isVibrating) {
+    return;
+  }
+  isVibrating = true;
+
+  let prevTotalTime = 0;
+  let heartBeatPattern;
+
+  while (!isOver) {
+    heartBeatPattern = setUpPattern(heartInfo.slice(prevIndex, nextIndex));
+
+    if (nextIndex >= heartInfo.length) {
+      isOver = true;
+    }
+
+    setTimeout(() => {
+      navigator.vibrate(heartBeatPattern);
+    }, prevTotalTime);
+    prevTotalTime += heartBeatPattern.reduce((prev, curr) => prev + curr);
+
+    prevIndex += INCREMENT_INDEX;
+    nextIndex += INCREMENT_INDEX;
+  }
 });
